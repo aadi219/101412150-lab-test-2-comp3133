5fc66adaff5d6a0a6768c94a9b493d208231d3b1
Aadi <aaadibadola@gmail.com>
2025-04-02 18:20:42 -0400

    app integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MissionListComponent } from './mission-list/mission-list.component';
+import { MissionDetailsComponent } from './mission-details/mission-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: MissionListComponent
+    },
+    {
+        path: 'details/:id', component: MissionDetailsComponent
+    }
+];
